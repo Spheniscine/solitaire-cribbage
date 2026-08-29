8dcff59b07d1ee3711c5e94cf7a2641e5f94ebb0
Spheniscine <spheniscine@gmail.com>
2026-08-29 20:31:20 +0800

    comment

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ const STATIC_CSS: bool = !cfg!(debug_assertions);
 
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const MAIN_CSS_STR: &str = const_css_minify::minify!("../assets/main.css");
-const _: &str = include_str!("../assets/main.css");
+const _: &str = include_str!("../assets/main.css"); // ensures recompilation if CSS changed
 
 mod game;
 mod components;
